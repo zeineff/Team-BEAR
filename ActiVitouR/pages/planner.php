6088e6a5c7c6b60af38e02aa683f2f2ffea4c511
pretentiousSloth <44803825+pretentiousSloth@users.noreply.github.com>
2019-11-13 20:56:42 +0000

    Add files via upload

--- ActiVitouR/pages/planner.php
+++ ActiVitouR/pages/planner.php
@@ -5,8 +5,12 @@
 
 <main>
 	<div id="main_box">
-		planner
+		<div id="google_map" style="width:100%;height:400px;"></div>
 	</div>
+
+	<script src="../js/google_maps.js"></script>
+
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBgQnfoVMX94wzFbhksI_pG-jWVYYAVe50&callback=initMap"></script>
 </main>
 
 <?php 
