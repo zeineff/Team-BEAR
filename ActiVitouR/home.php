0c9ce7c2e74024209d5d54a9b4be266c1ef30116
Bobbie Omobolabo Teriba <bobbieteriba@gmail.com>
2020-01-24 18:31:52 +0000

    Glendalough forest img and description

--- ActiVitouR/home.php
+++ ActiVitouR/home.php
@@ -18,16 +18,16 @@
                         <!-- Blog Entries Column -->
                         <div class="col-md-8">
 
-                            <h1 class="my-4">Page Heading
-                                <small>Secondary Text</small>
+                            <h1 class="my-4">Wicklow Mountains 
+                                <small>Co. Wicklow, Ireland</small>
                             </h1>
 
                             <!-- Blog Post -->
                             <div class="card mb-4">
-                                <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+                                <img class="card-img-top" src="img/gforest.jpg" alt="Card image cap">
                                 <div class="card-body">
-                                    <h2 class="card-title">Post Title</h2>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+                                    <h2 class="card-title">Glendalough Forest</h2>
+                                    <p class="card-text">Vist the Glendalough forest in wicklow mountains, The Roundtower and Lough Tay.</p>
                                     <a href="#" class="btn btn-primary">Read More &rarr;</a>
                                 </div>
                             </div>
